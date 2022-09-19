5ba94e0c2d0bac9eccdb1f6327b0ead72be2f356
Julia Nemell <julia@MacBookBro.local>
2022-09-19 14:00:45 +0200

    possible to getAdress from long and lat

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,19 @@
 import Container from 'react-bootstrap/Container'
-
+import useLatLng from '../hooks/useLatLng'
 
 const HomePage = () => {
+	let lat = "40.714224"
+	let lng = "-73.961452"
+	const { data, error, isError, isLoading } = useLatLng(lat, lng)
+	console.log('data', data)
+
 
 	return (
 		<Container className="py-3">
 			<h1 className="display-1">😂</h1>
 
 			<p className="display-2">Hallå!</p>
+			{data && <p>vi har data - adress: {data.results[0].formatted_address}</p>}
 
 		</Container>
 	)
--- src/services/GoogleMapsAPI.js
+++ src/services/GoogleMapsAPI.js
@@ -4,6 +4,27 @@
 
 import axios from 'axios'
 
-const API_KEY = NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
+axios.defaults.baseURL = "https://maps.googleapis.com/maps/api/geocode"
+const API_KEY = import.meta.env.VITE_GOOGLEMAPS_API_KEY
+
+/* 
+*  GET an endpoint function
+*  returns a promise
+ */
+const get = async (endpoint) => {
+	const response = await axios.get(endpoint)
+	return response.data
+}
 
 
+/* 
+*  GET the address for longitude and latitude
+	address as a place id
+ */
+	const getAddress = (lat, lng) => {
+		return get(`json?latlng=${lat},${lng}&key=${API_KEY}`)
+	}
+
+export default {
+	getAddress,
+}
