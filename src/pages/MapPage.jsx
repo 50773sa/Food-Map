407f8d435115db6be258a3fa4e985053b8177676
50773sa <93046590+50773sa@users.noreply.github.com>
2022-09-21 16:44:15 +0200

    Imported restaurant hook.

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,21 +1,27 @@
 import { useLoadScript } from "@react-google-maps/api";
+import { Alert } from "bootstrap";
+import { Container } from "react-bootstrap";
 import ShowMap from "../components/ShowMap";
+import useGetRestaurants from "../hooks/useGetRestaurants";
 
 
 const MapPage = () => {
   	const { isLoaded } = useLoadScript({
    		googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
-    
   	})
+	const {data: restaurantQuery , isError, isSuccess} = useGetRestaurants()
 
  	return (
-    	<>
+
+    	<Container>
 			{!isLoaded && <div>Loading...</div>}
 
-    		{isLoaded && (
-           		<ShowMap  />
+			{isError && <Alert variant='warning'>Error...</Alert>}
+
+    		{isSuccess && (
+				<ShowMap query={restaurantQuery}/>
     		)}	
-    	</>
+    	</Container>
   	)
 }
 
