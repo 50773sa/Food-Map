0748498307f3c9a6660699fb7d275ba16b111bdf
50773sa <93046590+50773sa@users.noreply.github.com>
2022-09-25 11:38:29 +0200

    Show marker for each restaurant.

--- src/components/ShowMap.jsx
+++ src/components/ShowMap.jsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react"
 import { GoogleMap,  MarkerF } from "@react-google-maps/api"
-import RestaurantList from "./RestaurantList"
+import Markers from "./Markers"
+import useGetRestaurants from "../hooks/useGetRestaurants"
+import MarkerItem from "./MarkerItem"
 
-const showMap = ({data, restaurants}) => {
+
+const showMap = () => {
+	const [markerPos, setMarkerPos] = useState()
 	const [currentPosition, setCurrentPosition] = useState({
 		lat: 55.603075505110425, 
 		lng: 13.00048440435288,
 	})
-	
+	const {data: restaurants, isSuccess, isError} = useGetRestaurants()
+
+	const mark = restaurants.map(rest => rest.position)
+
+
 	// Find  and set user's position
-	const onSuccess = async (pos) => {
+	const onSuccess = (pos) => {
 		const positionCords  = {
 			lat: pos.coords.latitude,
 			lng: pos.coords.longitude,
@@ -19,23 +27,30 @@ const showMap = ({data, restaurants}) => {
 
 	useEffect(() => {
 		navigator.geolocation.getCurrentPosition(onSuccess)
-	}, [])
+
+		setMarkerPos(mark)
 	
-	
+	},[])	
+
+	console.log('MARKER POS', markerPos)
+
 	return (
     	<GoogleMap 
 			mapContainerClassName="map-container vh-100"
 			zoom={13} 
 			center={currentPosition} 
 		>
+
 			{currentPosition.lat && (
 				<MarkerF position={currentPosition} />
 			)}
 
-			<RestaurantList data={data}/>
+			{markerPos && markerPos.map((rest) => (
+				<MarkerF key={rest.id} position={{lat: rest.latitude, lng: rest.longitude}}/>
+			))}
 
      	</GoogleMap>
-    );
+    )
 }
 export default showMap
 
