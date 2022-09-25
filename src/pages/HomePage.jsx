4ac12e84a8d6cc8b754d89c0cb4a7c2da7f6cf3d
Julia Nemell <julia@MacBookBro.lan>
2022-09-25 14:47:11 +0200

    added links to restaurantPage and mapPage on homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,16 @@
 import Container from 'react-bootstrap/Container'
-import useLatLng from '../hooks/useLatLng'
-import useCity from '../hooks/useCity'
+import RestaurantsPage from '../pages/RestaurantsPage'
+import MapPage from '../pages/MapPage'
+import { Link } from 'react-router-dom'
 
 const HomePage = () => {
-	let lat = "40.714224"
-	let lng = "-73.961452"
-	const { data: address, isLoading: isLoadingAddress, isError: isErrorAddress } = useLatLng(lat, lng)
-	const { data: city, isLoading: isLoadingCity, isError: isErrorCity } = useCity(lat, lng)
 
 	return (
 		<Container className="py-3">
-			<h1 className="display-1">😂</h1>
-
-			<p className="display-2">Hallå!</p>
-			{isLoadingAddress && <p>Loading...</p>}
-			{isErrorAddress && <p>Error</p>}
-			{address && <p>vi har adressen - adress: {address.results[0].formatted_address}</p>}
-
-			{isLoadingCity && <p>Loading city</p>}
-			{isErrorCity && <p>Error</p>}
-			{city && <p>vi har staden - stad: {city.results[0].address_components[0].long_name}</p>}
-
+			<div className="d-flex justify-content-center">
+				<Link className="m-4" to="/restaurants">See all restaurants</Link>
+				<Link className="m-4" to="/map">Go to the map</Link>
+			</div>
 		</Container>
 	)
 }
