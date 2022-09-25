a56d81a4adfffb56490594a0b709220c8cdcc7e2
Julia Nemell <julia@MacBookBro.lan>
2022-09-25 14:04:22 +0200

    started using react hook form for real

--- src/components/AddRestaurantForm.jsx
+++ src/components/AddRestaurantForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react'
+import { useState } from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
@@ -14,52 +14,38 @@ import { toast } from 'react-toastify'
 const AddRestaurantForm = () => {
 	const [error, setError] = useState(null)
 	const [loading, setLoading] = useState(false)
-	const [message, setMessage] = useState(null)
 	const [address, setAddress] = useState(null)
-	const { handleSubmit, formState: { errors }, reset } = useForm()
-	const { data: addressData, isLoading, isError } = useAddress(address)
-	
-	const nameRef = useRef()
-	const streetRef = useRef()
-	const postcodeRef = useRef()
-	const cityRef = useRef()
-	const restaurantInfoRef = useRef()
-	const restaurantTypeRef = useRef()
-	const foodTypeRef = useRef()
-	const emailRef = useRef()
-	const phoneRef = useRef()
-	const websiteRef = useRef()
-	const facebookRef = useRef()
-	const instagramRef = useRef()
+	const { handleSubmit, formState: { errors }, reset, register } = useForm()
+	const { data: addressData } = useAddress(address)
 
-	const createRestaurant = async (formdata) => {
-		console.log('data', formdata)
-		//get longitud and latitude from address
-		setAddress(streetRef.current.value)
+	const createRestaurant = async (data) => {
+		console.log('data', data)
 		
-		// the reference to the collection in the database
-		const restaurantsRef = collection(db, 'restaurants')
+		//get longitud and latitude from address
+		setAddress(data.street)
+		
 
 		//make firestore doc to store in the DB
-		await addDoc(restaurantsRef, {
+		await addDoc(collection(db, 'restaurants'), {
 			created: serverTimestamp(),
-			name: nameRef.current.value,
+			name: data.name,
 			address: {
-				street: streetRef.current.value,
-				postcode: postcodeRef.current.value,
-				city: cityRef.current.value,
+				street: data.street,
+				postcode: data.postcode,
+				city: data.city,
 			},
 			restaurant_info: {
-				restaurantInfo: restaurantInfoRef.current.value,
-				restaurantType: restaurantTypeRef.current.value,
-				foodType: foodTypeRef.current.value,
+				restaurantInfo: data.restaurantInfo,
+				restaurantType: data.restaurantType,
+				restaurantSort: data.restaurantSort,
+				cuisine: data.cuisine,
 			},
 			social: {
-				email: emailRef.current.value,
-				phone: phoneRef.current.value,
-				website: websiteRef.current.value,
-				facebook: facebookRef.current.value,
-				instagram: instagramRef.current.value,
+				email: data.email,
+				phone: data.phone,
+				website: data.website,
+				facebook: data.facebook,
+				instagram: data.instagram,
 			},
 			position: {
 				latitude: addressData.results[0].geometry.location.lat,
@@ -69,7 +55,8 @@ const AddRestaurantForm = () => {
 		})
 
 		toast.success('Tack för tipset!')
-		//reset()
+		setLoading(false)
+		reset()
 	}
 
 	return (
@@ -79,112 +66,176 @@ const AddRestaurantForm = () => {
 					<Card.Header as="h5">Tipsa oss!</Card.Header>
 					<Card.Body>
 						{error && (<Alert variant="danger">{error}</Alert>)}
-						{message && (<Alert variant="success">{message}</Alert>)}
 
-						<Form onSubmit={handleSubmit(createRestaurant)}>
+						<Form onSubmit={handleSubmit(createRestaurant)} noValidate>
 							<Form.Group className="mb-3">
 								<Form.Label>Namn*</Form.Label>
-								<Form.Control ref={nameRef} type="text" placeholder="Namn på stället" required />
+								<Form.Control 
+									{...register("name", {
+										required: "You must enter a name of the place",
+										minLength: {
+											value: 2,
+											message: "That's too short be a name!"
+										}
+									})}
+									type="text" placeholder="Namn på stället" />
+									{errors.name && <div className="invalid">{errors.name.message}</div>}
 							</Form.Group>
 
 							<Form.Group className="mb-3">
 								<Row>
 									<Col>
 										<Form.Label>Adress*</Form.Label>
-										<Form.Control type="text" placeholder="Adress" ref={streetRef} required />
+										<Form.Control 
+											{...register("street", {
+												required: "You must enter a address so we can find the place",
+												minLength: {
+													value: 4,
+													message: "That's too short be an address!"
+												}
+											})}
+											type="text" placeholder="Adress" />
+										{errors.street && <div className="invalid">{errors.street.message}</div>}
 									</Col>
 								</Row>
 								<Row className="mt-2">
 									<Col>
-										<Form.Control type="number" placeholder="Postnummer" ref={postcodeRef} required />
-										
+										<Form.Control 
+											{...register("postcode", {
+												required: "You must enter a postcode",
+												minLength: {
+													value: 5,
+													message: "That's too short be an address!"
+												}
+											})}
+											type="number" placeholder="Postnummer" />
+											{errors.postcode && <div className="invalid">{errors.postcode.message}</div>}
 									</Col>
 									<Col>
-										<Form.Control type="text" placeholder="Stad" ref={cityRef} required />
-										
+										<Form.Control 
+											{...register("city", {
+												required: "You must enter a city",
+												minLength: {
+													value: 2,
+													message: "That's not a city!"
+												}
+											})}
+											type="text" placeholder="Stad" />
+											{errors.city && <div className="invalid">{errors.city.message}</div>}
 									</Col>
 								</Row>
 							</Form.Group>
 
 							<Form.Group className="mb-3">
 								<Form.Label>Info om restaurangen*</Form.Label>
-								<Form.Control as="textarea" rows={2} type="text" placeholder="Skriv något om restaurangen" ref={restaurantInfoRef} required />
+								<Form.Control 
+									{...register("restaurantInfo", {
+										required: "Please write something about the place",
+										minLength: {
+											value: 2,
+											message: "We'd like some more information please..."
+										}
+									})}
+									as="textarea" rows={2} type="text" placeholder="Skriv något om restaurangen" />
+								{errors.restaurantInfo && <div className="invalid">{errors.restaurantInfo.message}</div>}
+
 							</Form.Group>
 							
 							<Form.Group className="mb-3">
 								<Form.Label>Vad är det för typ av ställe*</Form.Label>
-								<Form.Select ref={restaurantTypeRef}>
+								<Form.Select {...register("restaurantType")}>
 									<option>Café</option>
 									<option>Restaurang</option>
 									<option>Snabbmat</option>
 									<option>Kiosk/Grill</option>
 									<option>Food truck</option>
 								</Form.Select>
+								{errors.restaurantType && <div className="invalid">{errors.restaurantType.message}</div>}
+
 							</Form.Group>
 
 							<Form.Group className="mb-3">
 								<Form.Label>Utbud*</Form.Label>
 								<Form.Check
+									{...register("restaurantSort")}
 									inline
 									label="Lunch"
+									value="Lunch"
 									type="checkbox"
 									id="lunch"
 									className="mx-2"
 								/>
 								<Form.Check
+									{...register("restaurantSort")}
 									inline
 									label="After work"
+									value="After work"
 									type="checkbox"
 									id="after-work"
 									className="mx-2"
 								/>
 								<Form.Check
+									{...register("restaurantSort")}
 									inline
 									label="A la carte"
+									value="A la carte"
 									type="checkbox"
 									id="a-la-carte"
 									className="mx-2"
 								/>
 								<Form.Check
+									{...register("restaurantSort")}
 									inline
 									label="Bar"
+									value="Bar"
 									type="checkbox"
 									id="bar"
 									className="mx-2"
 								/>
+								{errors.restaurantSort && <div className="invalid">{errors.restaurantSort.message}</div>}
+
 							</Form.Group>
 
 							<Form.Group className="mb-3">
 								<Form.Label>Vad för slags kök?*</Form.Label>
-								<Form.Control type="text" placeholder="T ex franskt, thailändskt ..." ref={foodTypeRef} required />
+								<Form.Control 
+									{...register("cuisine", {
+										required: "Please write what kind of cuisine that is served here",
+										minLength: {
+											value: 2,
+											message: "We'd like some information about the cuisine please..."
+										}
+									})}
+									type="text" placeholder="T ex franskt, thailändskt ..." />
+								{errors.cuisine && <div className="invalid">{errors.cuisine.message}</div>}
 							</Form.Group>
 
 							<Form.Group id="email" className="mb-3">
 								<Form.Label>Email</Form.Label>
-								<Form.Control type="email"  ref={emailRef} />
+								<Form.Control {...register("email")} type="email" />
 							</Form.Group>
 
 							<Form.Group id="phone" className="mb-3">
 								<Form.Label>Telefonnummer</Form.Label>
-								<Form.Control type="number"  ref={phoneRef} />
+								<Form.Control {...register("phone")} type="number" />
 							</Form.Group>
 
 							<Form.Group id="website" className="mb-3">
 								<Form.Label>Hemsida</Form.Label>
-								<Form.Control type="text" ref={websiteRef} />
+								<Form.Control {...register("website")} type="text" />
 							</Form.Group>
 
 							<Form.Group id="facebook" className="mb-3">
 								<Form.Label>Facebook</Form.Label>
-								<Form.Control type="text" ref={facebookRef} />
+								<Form.Control {...register("facebook")} type="text" />
 							</Form.Group>
 
 							<Form.Group id="instagram" className="mb-3">
 								<Form.Label>Instagram</Form.Label>
-								<Form.Control type="text" ref={instagramRef} />
+								<Form.Control {...register("instagram")} type="text" />
 							</Form.Group>
 
-							<Button disabled={loading} type="submit">Skicka in förslag till oss!</Button>
+							<Button disabled={loading} type="submit">Skicka in förslaget till oss!</Button>
 						</Form>
 					</Card.Body>
 				</Card>
