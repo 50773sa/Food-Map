a2ccb74ac6336acabf6c615983658052e7ef7732
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-09-27 22:33:39 +0200

    Installed React table and added a temporary

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,20 +1,65 @@
+import { useMemo } from 'react'
 import { useAuthContext } from '../contexts/AuthContext'
 import { Container, Row, Col } from 'react-bootstrap'
-import AddAdmin from '../components/AddAdmin'
+// import AddAdmin from '../components/AddAdmin'
+import useGetRestaurants from '../hooks/useGetRestaurants'
+import BasicTable from '../components/BasicTable'
+import LoadingSpinner from '../components/LoadingSpinner'
+
 
 const AdminPage = () => {
+    const {data, isLoading} = useGetRestaurants()
     const { currentUser } = useAuthContext()
+
+    console.log(data)
+
+    const columns = useMemo(() => {
+        return [
+            {
+                Header: 'Namn',
+                accessor: 'name',
+            },
+            {
+                Header: 'Stad',
+                accessor: 'address.city',
+            },
+            {
+                Header: 'Address',
+                accessor: 'address.street',
+            },
+            {
+                Header: 'Cuisine',
+                accessor: 'restaurant_info.cuisine',
+            },
+            {
+                Header: 'Sort',
+                accessor: 'restaurant_info.restaurantSort',
+            },
+            {
+                Header: 'Typ',
+                accessor: 'restaurant_info.restaurantType',
+            },
+        ]
+    },[])
 
     return (
             <>
                 <div>
                     <p>You are logged in as {currentUser.email}</p> 
                 </div>
-
-                <Container className='d-flex flex-column justify-content-center flex-grow-1'>
+                
+                <Container className='d-flex flex-column justify-content-center align-items-center flex-grow-1'>
                     <Row>
-                        <Col md={6} className='m-auto'>
+                        {/* <Col md={12}>
                             <AddAdmin />
+                        </Col> */}
+                        <Col md={12}>
+                            {isLoading && (
+                                <LoadingSpinner />
+                            )}
+                            {!isLoading && data && 
+                                <BasicTable columns={columns} data={data} />
+                            }
                         </Col>
                     </Row>
                 </Container>
