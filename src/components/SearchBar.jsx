ef8977d79a2aec5574e1418e26ae3a4bc9b35174
Julia Nemell <julia@MacBookBro.lan>
2022-09-28 15:38:34 +0200

    restricted search to cities within Sweden and selected city is possible to reach from MapPage

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,31 +2,43 @@ import { useRef } from 'react'
 import { Autocomplete } from '@react-google-maps/api'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import GoogleMapsAPI from '../services/GoogleMapsAPI'
 
-const SearchBar = () => {
+const SearchBar = ({ setSelected }) => {
 	const searchRef = useRef()
 
-	const handleForm = (e) => {
+	const handleSearch = async (e) => {
 		e.preventDefault()
 
-		console.log(searchRef.current.value)
-		//onSubmit(searchRef.current.value)
+		//set the value to be what was searched for
+		const data = await GoogleMapsAPI.getCoordinates(searchRef.current.value)
+
+		if (data) {
+			setSelected(data.results[0].geometry.location)
+		} else {
+			return
+		}
 	}
 
 	return (
 		<div>
-			<Form onSubmit={handleForm}>
-				<Form.Group className="d-flex justify-content-center">
-					<Autocomplete>
+			<Form onSubmit={handleSearch}>
+				<Autocomplete restrictions={{ 'country': ['SE'] }} types={["locality"]}>
+					<Form.Group className="d-flex justify-content-center">
 						<Form.Control
 							type="text"
 							ref={searchRef}
-							placeholder="Enter an adress"
+							placeholder="Sök på en stad"
 							required
 						/>
-					</Autocomplete>
-					<Button type="submit" variant="outline-primary">Search</Button>
-				</Form.Group>
+
+						<Button type="submit" variant="outline-secondary">
+							<FontAwesomeIcon icon={faSearch} />
+						</Button>
+					</Form.Group>
+				</Autocomplete>
 			</Form>
 		</div>
 	)
