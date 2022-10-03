afd326e6d8c7c75ad04913953215f4675c36e553
Julia Nemell <julia@MacBookBro.lan>
2022-10-03 13:54:51 +0200

    listed restaurants sorted by name

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,16 +1,24 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import RestaurantListItem from './RestaurantListItem'
+import Button from 'react-bootstrap/Button'
 
 const RestaurantList = ({ data }) => {
+	
 	return (
-		<Row xs={1} sm={2} md={2} lg={3}>
-			{data.map((restaurant, i) => (
-				<Col className="my-3" key={i}>
-					<RestaurantListItem restaurant={restaurant} />
-				</Col>
-			))}
-		</Row>
+		<>
+			<Button>Sort by name</Button>
+			<Row xs={1} sm={2} md={2} lg={3}>
+				{data
+					.sort((a, b) => a.name.localeCompare(b.name))
+					.map((restaurant, i) => (
+						<Col className="my-3" key={i}>
+							<RestaurantListItem restaurant={restaurant} />
+						</Col>
+					))
+				}
+			</Row>
+		</>
 	)
 }
 
