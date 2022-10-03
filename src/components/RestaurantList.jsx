e6937f02a18582874a92579009132dcae74fa651
Julia Nemell <julia@MacBookBro.lan>
2022-10-03 14:03:01 +0200

    listed restaurants sorted by name

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,13 +1,11 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import RestaurantListItem from './RestaurantListItem'
-import Button from 'react-bootstrap/Button'
 
 const RestaurantList = ({ data }) => {
 	
 	return (
 		<>
-			<Button>Sort by name</Button>
 			<Row xs={1} sm={2} md={2} lg={3}>
 				{data
 					.sort((a, b) => a.name.localeCompare(b.name))
