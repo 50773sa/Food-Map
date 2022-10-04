75fb72d07c6c1b1bda46d3f9eba2f51c5514025e
nddesigns <56118523+nikodimi@users.noreply.github.com>
2022-10-04 23:02:50 +0200

    Now able to sort approved restaurant by name

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,16 +1,40 @@
+import { useState } from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import RestaurantListItem from './RestaurantListItem'
 
 const RestaurantList = ({ data }) => {
+    const [sortedData, setSortedData] = useState(data); 
+    const [order, setOrder] = useState("ASC"); 
+    
+ const sorting = (value) => {
+    if (order === "ASC") {
+        const sorted = [...data].sort((a,b) =>
+            a[value] > b[value] ? 1 : -1
+        )
+        setSortedData(sorted)
+        setOrder('DSC')
+    }
+    if (order === "DSC") {
+        const sorted = [...data].sort((a,b) =>
+            a[value] < b[value] ? 1 : -1
+        )
+        setSortedData(sorted)
+        setOrder('ASC')
+    }
+}
+
 	return (
-		<Row xs={1} sm={2} md={2} lg={3}>
-			{data.map((restaurant, i) => (
-				<Col className="my-3" key={i}>
-					<RestaurantListItem restaurant={restaurant} />
-				</Col>
-			))}
-		</Row>
+        <>
+            <span>Sortera på namn</span><span className="sorterings-knapp" onClick={()=>sorting("name")}> {order === "ASC" ? "↓" : "↑"}</span> 
+            <Row className="restaurant-wrapper">
+                {sortedData.map((res, i) => (
+                    <Col md={4} className="my-3" key={i}>
+                        <RestaurantListItem restaurant={res} />
+                    </Col>
+                ))}
+            </Row>
+        </>
 	)
 }
 
