18b87daabf0ef15322530bbc8f4913621fb5578e
50773sa <93046590+50773sa@users.noreply.github.com>
2022-10-04 17:13:04 +0200

    Minor changes in styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,6 +7,7 @@ import { faPhone, faGlobe } from '@fortawesome/free-solid-svg-icons'
 import Card from 'react-bootstrap/Card'
 import Offcanvas from 'react-bootstrap/Offcanvas'
 import Button from "react-bootstrap/Button"
+import defaultPhoto from "../assets/Images/defaultPhoto.jpg"
 
 
 
@@ -21,12 +22,12 @@ const Sidebar = ({ show, closeInfoBox, selectedRestaurant }) => {
 			<Offcanvas.Header className='d-flex justify-content-end' onClick={closeInfoBox} closeButton> </Offcanvas.Header>
 
 			<Card style={{ width: '100%' }}>
-				<Card.Img variant="top" src="https://via.placeholder.com/350x200?text=Kittlar+dödsskönt+i+kistan" />
+				<Card.Img variant="top" src={selectedRestaurant.url !== null ? selectedRestaurant.url : defaultPhoto} />
 
 				<Card.Body>
 					<Card.Title>{selectedRestaurant.name} </Card.Title>
 
-					<Card.Subtitle className="mb-3">{selectedRestaurant.restaurant_info.cuisine}</Card.Subtitle>
+					<Card.Subtitle className="mb-3 cuisine">{selectedRestaurant.restaurant_info.cuisine}</Card.Subtitle>
 
 					<Card.Text>
 						{
@@ -38,39 +39,40 @@ const Sidebar = ({ show, closeInfoBox, selectedRestaurant }) => {
 
 					<hr/>
 
-					<Card.Text className="d-flex justify-content-between" style={{ color: 'black'}}>
+					<Card.Text className="d-flex justify-content-end" style={{ color: 'black'}}>
 						{selectedRestaurant.social.facebook && (
-							<a href={selectedRestaurant.social.facebook} target="_blank">
+							<a href={selectedRestaurant.social.facebook} className="" target="_blank">
 								<FontAwesomeIcon icon={faFacebook} />
 							</a>
 						)}
 
 						{selectedRestaurant.social.instagram && (
-							<a href={selectedRestaurant.social.instagram} target="_blank">
+							<a href={selectedRestaurant.social.instagram} className="" target="_blank">
 								<FontAwesomeIcon icon={faInstagram} />
 							</a>
 						)}
 
 						{selectedRestaurant.social.website && (
-							<a href={selectedRestaurant.social.website} target="_blank">
+							<a href={selectedRestaurant.social.website} className="" target="_blank">
 								<FontAwesomeIcon icon={faGlobe} />
 							</a>
 						)}
 
 						{selectedRestaurant.social.phone && (
-							<a href={"tel:" + selectedRestaurant.social.phone}>
+							<a href={"tel:" + selectedRestaurant.social.phone} className="" >
 								<FontAwesomeIcon icon={faPhone} />
 							</a>
 						)}
 
 						{selectedRestaurant.social.email && (
-							<a href={"mailto:" + selectedRestaurant.social.email}>
+							<a href={"mailto:" + selectedRestaurant.social.email} className="">
 								<FontAwesomeIcon icon={faEnvelope} />
 							</a>
 						)}
 					</Card.Text>
 					<hr/>
 
+					<Card.Text className='mb-1'><strong>Beskrivning</strong></Card.Text>
 					<Card.Text>{selectedRestaurant.restaurant_info.restaurantInfo}</Card.Text>
 					<hr/>
 				</Card.Body>
