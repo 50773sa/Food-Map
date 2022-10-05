b3b5fb73980d69ddb740b76d38ba2ce15f370ba2
Julia Nemell <julia@MacBookBro.lan>
2022-10-05 16:10:28 +0200

    removed filter search in all restaurants page

--- src/pages/RestaurantsPage.jsx
+++ src/pages/RestaurantsPage.jsx
@@ -1,14 +1,12 @@
 import RestaurantList from '../components/RestaurantList'
 import useGetApprovedRestaurants from '../hooks/useGetApprovedRestaurants'
 import Container from 'react-bootstrap/Container'
-import SearchFilterBar from '../components/SearchFilterBar'
 
 const RestaurantsPage = () => {
 	const {data, isLoading} = useGetApprovedRestaurants()
 
 	return (
 		<Container>
-			<SearchFilterBar data={data}/>
 			{isLoading && (<p>Loading data...</p>)}
 			{!isLoading && data && <RestaurantList data={data}/>}
 		</Container>
