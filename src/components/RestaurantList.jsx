fb9732b66f278d69b1e45934dacbba181d4c639b
Julia Nemell <jnemell@gmail.com>
2022-12-13 15:16:35 +0100

    Adds a useEffect to fetch all the cities where we have restaurants and save the cities in a useState

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -2,27 +2,45 @@ import { useState } from 'react'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import RestaurantListItem from './RestaurantListItem'
+import { useEffect } from 'react'
 
-const RestaurantList = ({ data }) => {
-    const [sortedData, setSortedData] = useState(data); 
-    const [order, setOrder] = useState("ASC"); 
+const RestaurantList = ({ approvedRestaurants }) => {
+    const [sortedData, setSortedData] = useState(approvedRestaurants); 
+    const [order, setOrder] = useState("ASC");
+    const [cities, setCities] = useState([])
+    console.log('approvedRestaurants', approvedRestaurants)
     
- const sorting = (value) => {
-    if (order === "ASC") {
-        const sorted = [...data].sort((a,b) =>
-            a[value].toLowerCase() > b[value].toLowerCase() ? 1 : -1
-        )
-        setSortedData(sorted)
-        setOrder('DSC')
+    const sorting = (value) => {
+        if (order === "ASC") {
+            const sorted = [...approvedRestaurants].sort((a,b) =>
+                a[value].toLowerCase() > b[value].toLowerCase() ? 1 : -1
+            )
+            setSortedData(sorted)
+            setOrder('DSC')
+        }
+        if (order === "DSC") {
+            const sorted = [...approvedRestaurants].sort((a,b) =>
+                a[value].toLowerCase() < b[value].toLowerCase() ? 1 : -1
+            )
+            setSortedData(sorted)
+            setOrder('ASC')
+        }
     }
-    if (order === "DSC") {
-        const sorted = [...data].sort((a,b) =>
-            a[value].toLowerCase() < b[value].toLowerCase() ? 1 : -1
-        )
-        setSortedData(sorted)
-        setOrder('ASC')
-    }
-}
+
+    useEffect(() => {
+        const getCities = () => {
+            let citiesWithRestaurants = []
+            approvedRestaurants.map(res => {
+                let cityCheck = citiesWithRestaurants.includes(res.address.city)
+                if (cityCheck == false) {
+                    return citiesWithRestaurants.push(res.address.city)
+                }
+            })
+            setCities(citiesWithRestaurants)
+        }
+        getCities()
+    }, [approvedRestaurants])
+
 
 	return (
         <>
